94916967e854ea2c3f78dc27cc375d42b7b3b7c8
Biyam Khaiju <khaijub91@gmail.com>
2026-06-03 09:38:13 +0545

    wedding invitation

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
 
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+
 export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
   build: {
     outDir: "dist/client",
     emptyOutDir: true,
@@ -12,3 +16,4 @@ export default defineConfig({
     },
   },
 });
+
